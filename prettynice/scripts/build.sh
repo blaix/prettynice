b5e027065ed2c9812478d79e3ec545398c85fca4
Justin Blake <justin@blaix.com>
2024-02-19 06:52:01 -0500

    Move public assets from build.sh to prettynice cli

--- prettynice/scripts/build.sh
+++ prettynice/scripts/build.sh
@@ -25,27 +25,24 @@ mkdir -p dist/server
 mkdir -p server/.prettynice/Prettynice
 mkdir -p server/.prettynice/Gen/Components
 
-# public assets
-if [[ "$(ls public/)" ]]; then 
-    cp -r public/* dist/client/
-fi
-
 # prevent a 404 error if there's no client-side js to build
 # (the server renders a script tag no matter what. maybe fix that?)
 touch dist/client/main.js
 
-# build components
+# run the prettynice cli
+# (slowly migrating everything in this build.sh here)
+cd $PROJECT_ROOT/prettynice/cli
+npx gren make src/Main.gren
+mkdir -p build
+mv app build/
+cd $EXAMPLE_ROOT
+node $PROJECT_ROOT/prettynice/cli/build/app
+
 if [[ -d client/src/Components ]]; then
   if [[ "$(ls client/src/Components)" ]]; then
     mkdir -p client/.prettynice/Prettynice
     mkdir -p client/.prettynice/Gen/Components
     cp "$PROJECT_ROOT/src/Prettynice/Component.gren" client/.prettynice/Prettynice/
-    cd $PROJECT_ROOT/prettynice/cli
-    npx gren make src/Main.gren
-    mkdir -p build
-    mv app build/
-    cd $EXAMPLE_ROOT
-    node $PROJECT_ROOT/prettynice/cli/build/app
     cd $EXAMPLE_ROOT/client
     npx gren make $(find .prettynice/Gen/Components -name "*.gren") --output=../dist/client/main.js
     if [[ "$(ls src/Components/**/*.js 2>/dev/null)" ]]; then

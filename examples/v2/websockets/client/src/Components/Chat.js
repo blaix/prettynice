5658a1827562cb10631b65661d9e4d452d2416d1
Justin Blake <justin@blaix.com>
2024-12-22 23:23:13 -0500

    Add comments to port files in websocket example

--- examples/v2/websockets/client/src/Components/Chat.js
+++ examples/v2/websockets/client/src/Components/Chat.js
@@ -1,6 +1,8 @@
 const socket = new WebSocket("ws://localhost:8080");
 
 export function init(component) {
+    // Listen for messages from the websocket and send them to the
+    // gotMessage port defined in client/src/Components/Chat.gren
     socket.addEventListener("message", (event) => {
         component.ports.gotMessage.send(event.data);
     });

ce1d759aafd02972e6c8d6d7da4684a4984039a9
Justin Blake <justin@blaix.com>
2025-02-23 06:51:28 -0500

    cp website/next to website/v3

---
+++ website/v3/astro.config.mjs
@@ -0,0 +1,56 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import rehypeExternalLinks from 'rehype-external-links';
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Pretty Nice',
+            expressiveCode: {
+              plugins: [
+                pluginLineNumbers()
+              ],
+            },
+            customCss: [
+              './src/styles/custom.css',
+            ],
+			social: {
+				github: 'https://github.com/blaix/prettynice',
+				mastodon: 'https://hachyderm.io/@blaix',
+			},
+			sidebar: [
+				{
+					label: 'The Basics',
+					items: [
+						{ label: 'Getting Started', link: '/basics/start/' },
+						{ label: 'The Server', link: '/basics/server/' },
+						{ label: 'Client-side Components', link: '/basics/client/' },
+						{ label: 'Images and Styling', link: '/basics/assets/' },
+						{ label: 'Forms', link: '/basics/forms/' },
+					],
+				},
+			  {
+					label: 'Next Steps',
+					items: [
+						{ label: 'The Elm Architecture', link: '/next/tea/' },
+						{ label: 'Server-side Javascript', link: '/next/server-js/' },
+						{ label: 'Client-side Javascript', link: '/next/client-js/' },
+						{ label: 'Examples', link: '/next/examples/' },
+					],
+				},
+			  {
+					label: 'Guides',
+                    autogenerate: { directory: 'guides' },
+				},
+			],
+		}),
+	],
+  markdown: {
+    rehypePlugins: [
+      [ rehypeExternalLinks, {content: { type: 'text', value: ' ↗'}},
+      ],
+    ]
+  },
+});
